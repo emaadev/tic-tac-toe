feb04443aa7ee0e8720ab12bef76d9e4dc38f6dc
EmanuelNu <emanuel.nu02@gmail.com>
2023-10-10 12:02:31 -0300

    first commit

---
+++ src/components/TableGame.jsx
@@ -0,0 +1,21 @@
+import { Square } from "./Square.jsx"
+
+export function TableGame (updateBoard) {
+    return (
+            <section className='game'>
+                {
+                board.map((square, index) => {
+                    return (
+                    <Square
+                        key={index}
+                        index={index}
+                        updateBoard={updateBoard}
+                    >
+                        {square}
+                    </Square>
+                    )
+                })
+                }
+            </section>
+    )
+}
